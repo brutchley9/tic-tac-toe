141a01a7a0dcdd784b81b348a7b14dbd453952c9
brutchley <bhutchison0901@yahoo.com>
2024-03-02 13:26:40 -0600

    added functionality to each square tic tac toe, background changes color for each. Now to add logic to proceed with game

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function clickTopLeft() {
+    document.getElementById("topleft").style.backgroundColor = "red";
+};
+
+function clickTopMid() {
+    document.getElementById("topmid").style.backgroundColor = "red";
+};
+
+function clickTopRight() {
+    document.getElementById("topright").style.backgroundColor = "red";
+};
+
+function clickMidLeft() {
+    document.getElementById("midleft").style.backgroundColor = "red";
+};
+
+function clickMidMid() {
+    document.getElementById("midmid").style.backgroundColor = "red";
+};
+
+function clickMidRight() {
+    document.getElementById("midright").style.backgroundColor = "red";
+};
+
+function clickBottomLeft() {
+    document.getElementById("bottomleft").style.backgroundColor = "red";
+};
+
+function clickBottomMid() {
+    document.getElementById("bottommid").style.backgroundColor = "red";
+};
+
+function clickBottomRight() {
+    document.getElementById("bottomright").style.backgroundColor = "red";
+};
